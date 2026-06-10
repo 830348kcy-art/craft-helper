270907b9362aa12d06209dcaf69c15c71b48e25a
jhan6936-spec <jhan6936@gmail.com>
2026-06-11 02:06:25 +0900

    remove: 자동 농장 3D 뷰어 및 three.js 의존성 제거

--- lib/farm-textures.ts
+++
@@ -1,6 +0,0 @@
-/** @deprecated — farm-block-faces.ts 로 통합 */
-export {
-  getBlockFaceUrls,
-  isTransparentBlock as isTransparentFarmBlock,
-  farmBlockFallbackColor,
-} from "./farm-block-faces";
